eb27cb15653def56cda3ca38322aad31901b51c9
Marek Kobyliński <marek@niepsuj.it>
2016-04-18 00:30:58 +0200

    config path

--- src/ProjectProvider.php
+++ src/ProjectProvider.php
@@ -19,56 +19,52 @@ class ProjectProvider implements ServiceProviderInterface
 
 		$app['callback_resolver'] = function ($app) { return new Callbacks($app); };
 
-		if(isset($app['config.path'])){
-			if(!is_dir($app['config.path'])){
-				throw new \Exception('Invalid config path');
+		$app['config.path.resolve'] = $app->protect(function($path) use ($app){
+
+			if(!isset($app['config.path'])){
+				throw new \Exception('Missing config path');
 			}
 
-			$app['config.path.resolve'] = $app->protect(function($path) use ($app){
-				$base = $app['config.path'].'/'.$path;
+			$base = $app['config.path'].'/'.$path;
 
-				if($app['env'] !== 'production'){
-					$path = $base.'.'.$app['env'].'.json';
-					if(file_exists($path)){
-						return $path;
-					}
+			if($app['env'] !== 'production'){
+				$path = $base.'.'.$app['env'].'.json';
+				if(file_exists($path)){
+					return $path;
 				}
+			}
 
-				$path = $base.'.json';
-				if(!file_exists($path)){
-					throw new \Exception('Invalid config file: '.$path);
-				}
+			$path = $base.'.json';
+			if(!file_exists($path)){
+				throw new \Exception('Invalid config file: '.$path);
+			}
 
-				return $path;
-			});
+			return $path;
+		});
 
-			$app['config.load'] = $app->protect(function($path) use ($app){
-				$file = $app['config.path.resolve']($path);
-				$data = json_decode(file_get_contents($file), true);
-				$iterator = new \RecursiveArrayIterator($data);
+		$app['config.load'] = $app->protect(function($path) use ($app){
+			$file = $app['config.path.resolve']($path);
+			$data = json_decode(file_get_contents($file), true);
+			$iterator = new \RecursiveArrayIterator($data);
 
-				$keys = [preg_replace('/([^a-zA-Z0-9_\-]+)/', '.', $path)];
-				$iterate    = function($iterator) use (&$keys, &$iterate, $app){
-	                while( $iterator->valid() ) {
-	                    if( $iterator->hasChildren() ) {
-	                        array_push($keys, $iterator->key());
-	                        $iterate($iterator->getChildren());
-	                    }else{
-	                        $app[implode('.', $keys).'.'.$iterator->key()] = $iterator->current();
-	                    }
-	                    $iterator -> next(); 
-	                }
-	                array_pop($keys);
-	            };
+			$keys = [preg_replace('/([^a-zA-Z0-9_\-]+)/', '.', $path)];
+			$iterate    = function($iterator) use (&$keys, &$iterate, $app){
+                while( $iterator->valid() ) {
+                    if( $iterator->hasChildren() ) {
+                        array_push($keys, $iterator->key());
+                        $iterate($iterator->getChildren());
+                    }else{
+                        $app[implode('.', $keys).'.'.$iterator->key()] = $iterator->current();
+                    }
+                    $iterator -> next(); 
+                }
+                array_pop($keys);
+            };
 
-	            iterator_apply($iterator, $iterate, array($iterator));
-				return $data;
-			});
-		}else{
-			$app['config.load'] = $app->protect(function($path) {
-				throw new \Exception('Missing config path');
-			});
-		}
+            iterator_apply($iterator, $iterate, array($iterator));
+			return $data;
+		});
+	
 
 		$app['registry'] = $app->factory(function(){
 			return new Registry();
