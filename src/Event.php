11957d21e963630dbcb9ccb01c3b9580f2b4fafe
Marek Kobyliński <marek@niepsuj.it>
2016-05-09 01:13:03 +0200

    fix

--- src/Event.php
+++ src/Event.php
@@ -6,6 +6,8 @@ use Symfony\Component\EventDispatcher\Event as BaseEvent;
 
 class Event extends BaseEvent implements \ArrayAccess
 {
+	use StoreTrait;
+
 	public function __construct($data)
 	{
 		if(is_array($data)){
