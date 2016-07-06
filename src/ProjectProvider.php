8c57e5cf339b3911d11e1776a9eaef2080ee371d
Marek Kobyliński <marek@niepsuj.it>
2016-07-06 21:53:08 +0200

    json payload

--- src/ProjectProvider.php
+++ src/ProjectProvider.php
@@ -94,5 +94,12 @@ class ProjectProvider implements ServiceProviderInterface
 			if($app->booted)
             	return $app['dispatcher']->dispatch($name, new Event($data))->getData();
 		});
+
+		$app->before(function (Request $request) {
+			if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+				$data = json_decode($request->getContent(), true);
+				$request->request->replace(is_array($data) ? $data : array());
+			}
+		});
 	}
 }
